fc5fe7005c3bdaecca798f3a4dfcd40a150718d9
AndresFelipe23 <75282613+AndresFelipe23@users.noreply.github.com>
2024-03-15 13:37:30 -0500

    FINISH

--- obj/Debug/net6.0/TiendaOnline.AssemblyInfo.cs
+++ obj/Debug/net6.0/TiendaOnline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92950d870a134d270720a2e8157b2d6057aca1b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
